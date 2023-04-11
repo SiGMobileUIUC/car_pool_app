507796dc35fa2e148285e4b805c9625cc21fd6bf
amanshah <ashah305@illinois.edu>
2023-04-11 17:50:16 -0500

    New login screen

--- car_pool_app/lib/main.dart
+++ car_pool_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CARPOOL APP',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Carpool App'),
-    );
+        title: 'CARPOOL APP',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        //home: const MyHomePage(title: 'Carpool App'),
+        home: LoginDemo());
   }
 }
 
