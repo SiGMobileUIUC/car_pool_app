823a16baac03a4b142bcf792f4935d94561073ea
adrian-szwejk <aszwe2@illinois.edu>
2023-06-28 10:38:02 -0500

    Fixed the issue with unbound height for listview.builder as was inside SinglChildScrollView Implemented Sliver with SliverList/builderDelegate to simulate listview.builder but without unbound height issue

--- car_pool_app/lib/screens/account/account.dart
+++ car_pool_app/lib/screens/account/account.dart
@@ -605,11 +605,10 @@ class _AccountScreenState extends State<AccountScreen> {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
                 children: [
@@ -649,10 +648,133 @@ class _AccountScreenState extends State<AccountScreen> {
                 ],
               ),
             ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              childCount: 7,
+              (BuildContext context, int index) {
+                switch (index) {
+                  case 0:
+                    return ListTile(
+                      leading: const Icon(Icons.person),
+                      title: const Text('Name'),
+                      subtitle: Text(user.getName() ?? ''),
+                      trailing: const Icon(Icons.arrow_forward_ios),
+                      onTap: () {
+                        createEditButton("First Last", 0);
+                      },
+                    );
+                  case 1:
+                    return ListTile(
+                      leading: const Icon(Icons.info),
+                      title: const Text('Bio'),
+                      subtitle: Text(user.getBio() ?? ''),
+                      trailing: const Icon(Icons.arrow_forward_ios),
+                      onTap: () {
+                        createEditButton("School, Major, Year in School", 1);
+                      },
+                    );
+                  case 2:
+                    return ListTile(
+                      leading: const Icon(Icons.drive_eta_rounded),
+                      title: const Text('Role'),
+                      subtitle: Text(user.getRole() ?? ''),
+                      trailing: const Icon(Icons.arrow_forward_ios),
+                      onTap: () {
+                        createEditButton("Driver, Rider, or Both", 2);
+                      },
+                    );
+                  case 3:
+                    return ListTile(
+                      leading: const Icon(Icons.email),
+                      title: const Text('Email'),
+                      subtitle: Text(user.getEmail() ?? ''),
+                      trailing: const Icon(Icons.arrow_forward_ios),
+                      onTap: () {
+                        createEditButton("netid@illinois.edu", 3);
+                      },
+                    );
+                  case 4:
+                    return ListTile(
+                      leading: const Icon(Icons.phone),
+                      title: const Text('Phone Number'),
+                      subtitle: Text(user.getPhoneNumber() ?? ''),
+                      trailing: const Icon(Icons.arrow_forward_ios),
+                      onTap: () {
+                        createEditButton("### ### ####", 4);
+                      },
+                    );
+                  case 5:
+                    return ListTile(
+                      leading: const Icon(Icons.group),
+                      title: const Text('Instagram'),
+                      subtitle: Text(user.getSocialMedia() ?? ''),
+                      trailing: const Icon(Icons.arrow_forward_ios),
+                      onTap: () {
+                        createEditButton("@socialmedia", 5);
+                      },
+                    );
+                  case 6:
+                    return ListTile(
+                      leading: const Icon(Icons.lock),
+                      title: const Text('Password'),
+                      subtitle: const Text('********'),
+                      trailing: const Icon(Icons.arrow_forward_ios),
+                      onTap: () {
+                        createEditButton("*******", 6);
+                      },
+                    );
+                  default:
+                    return const SizedBox.shrink();
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+            if (currentIndex == 0) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomePage()),
+              );
+            } else if (currentIndex == 1) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const RidePage()),
+              );
+            }
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.drive_eta),
+            label: 'Rides',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Account',
+          ),
+        ],
+        selectedItemColor: Colors.black,
+      ),
+    );
+  }
+}
 
-           // Start of listview builder
-           ListView.builder(
-            itemCount: 7, 
+/*
+
+// Start of listview builder
+          ListView.builder(
+            itemCount: 7,
             itemBuilder: (BuildContext context, int index) {
               switch (index) {
                 case 0:
@@ -681,9 +803,9 @@ class _AccountScreenState extends State<AccountScreen> {
                     title: const Text('Role'),
                     subtitle: Text(user.getRole() ?? ''),
                     trailing: const Icon(Icons.arrow_forward_ios),
-                      onTap: () {
-                        createEditButton("Driver, Rider, or Both", 2);
-                      },
+                    onTap: () {
+                      createEditButton("Driver, Rider, or Both", 2);
+                    },
                   );
                 case 3:
                   return ListTile(
@@ -730,44 +852,5 @@ class _AccountScreenState extends State<AccountScreen> {
               }
             },
           ),
-          // End of listview builder
-        ],
-      ),
-    ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentIndex,
-        onTap: (index) {
-          setState(() {
-            currentIndex = index;
-            if (currentIndex == 0) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomePage()),
-              );
-            } else if (currentIndex == 1) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const RidePage()),
-              );
-            }
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.drive_eta),
-            label: 'Rides',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Account',
-          ),
-        ],
-        selectedItemColor: Colors.black,
-      ),
-    );
-  }
-}
+
+          */
